88d2e5e781ab6a815eefe83f52004ab40cd126f9
EricLago <ericlago18@gmail.com>
2021-04-16 01:03:07 +0800

    initial commit - intro to csharp oop

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PayrollSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string employeeA = "Michael Jackson";
+            string employeeB = "Bruno Mars";
+            string employeePositionStaff = "Staff";
+            string employeePositionHeadStaff = "Head Staff";
+            int staffSalary = 16000;
+            int headStaffSalary = 22000;
+
+            Console.WriteLine("Welcome to Payroll System");
+            Console.WriteLine("Here are the List's of Employees:");
+            Console.WriteLine("A: " + employeeA);
+            Console.WriteLine("B: " + employeeB);
+
+            Console.Write("If you wish to view the payslip of employee A type a and if employee B type b: ");
+            string optionSelected = Console.ReadLine().ToLower();
+            if (optionSelected.Equals("a")){
+                Console.WriteLine("Name: " + employeeA); 
+                Console.WriteLine("Job Position: " + employeePositionStaff);
+                Console.WriteLine("Monthly Salary: " + staffSalary);
+            }
+            else if (optionSelected.Equals("b")){
+                Console.WriteLine("Name: " + employeeB);
+                Console.WriteLine("Job Position: " + employeePositionHeadStaff);
+                Console.WriteLine("Monthly Salary: " + headStaffSalary);
+            }
+            else
+            {
+                Console.WriteLine("INVALID OPTION!!!");
+            }
+        }
+    }
+}
